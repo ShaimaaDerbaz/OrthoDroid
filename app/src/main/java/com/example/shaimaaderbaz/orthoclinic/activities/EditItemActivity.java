3ae8620f5792627225229b08aa0bb105da9ce740
ShaimaaDerbaz <shaimaa.derbaz@gmail.com>
2018-08-29 03:37:40 +0200

    edit item in list

---
+++ app/src/main/java/com/example/shaimaaderbaz/orthoclinic/activities/EditItemActivity.java
@@ -0,0 +1,13 @@
+package com.example.shaimaaderbaz.orthoclinic.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+    }
+}
