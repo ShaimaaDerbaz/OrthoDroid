e5a65a72f78b811a536eac41d5241ef9961e9e71
ShaimaaDerbaz <shaimaa.derbaz@gmail.com>
2018-08-30 18:17:38 +0200

    add update operations

---
+++ app/src/main/java/com/example/shaimaaderbaz/orthoclinic/activities/EditOperationActivity.java
@@ -0,0 +1,15 @@
+package com.example.shaimaaderbaz.orthoclinic.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shaimaaderbaz.orthoclinic.R;
+
+public class EditOperationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_operation);
+    }
+}
